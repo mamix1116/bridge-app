6134e40b34568440e9f2ad92c627947913439147
mamix1116 <mamix1116@gmail.com>
2021-01-02 14:07:26 +0900

    change app name to config

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,4 +1,4 @@
-"""app URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from uploads.views import image_upload
 
 urlpatterns = [
-    path('uploads', image_upload, name="uploads"),
     path('admin/', admin.site.urls),
 ]
 
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+                          
