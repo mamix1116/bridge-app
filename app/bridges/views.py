0b20ab397824916ebd3dcfe49a7c3835cfc949bb
mamix1116 <mamix1116@gmail.com>
2021-01-19 19:49:10 +0900

    create bridges list view and pagination

--- app/bridges/views.py
+++ app/bridges/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+from .models import Bridge
 
 # Create your views here.
 def index(request):
-    return render(request, 'bridges/bridges.html')
+    bridges = Bridge.objects.all()
 
-def bridge(request):
+    paginator = Paginator(bridges, 3) # items per page
+    page = request.GET.get('page')
+    paged_bridges = paginator.get_page(page)
+
+    context = {
+        'bridges': paged_bridges
+    }
+    return render(request, 'bridges/bridges.html', context)
+
+def bridge(request, bridges_id):
     return render(request, 'bridges/bridge.html')
 
 def search(request):
