ea0e6e7792b6bf669648f81dd093b1346565855f
mamix1116 <mamix1116@gmail.com>
2021-01-23 15:30:18 +0900

    add to get last inspection serializers

--- app/bridges/api/serializers.py
+++ app/bridges/api/serializers.py
@@ -4,9 +4,21 @@ from bridges.models import ManagementOrganization, Bridge
 class BridgeSerializer(serializers.ModelSerializer):
     # management_organization = ManagementOrganizationSerializer(read_only=True)
 
+    last_inspection = serializers.SerializerMethodField()
+
     class Meta:
         model = Bridge
-        fields = "__all__"
+        fields = ('id', 'bridge_id', 'bridge_name', 'bridge_name_yomi',
+                  'location_city', 'management_organization', 'last_inspection')
+
+    def get_last_inspection(self, obj):
+        try:
+            last_inspection = models.Inspection.objects.filter(bridge_id=obj.id).order_by('-date_inspect').first()
+        except Exception as e:
+            return None
+            if last_inspection is None:
+                return None
+            return InspectionSerializer(last_inspection).data
 
 
 class ManagementOrganizationSerializer(serializers.ModelSerializer):
