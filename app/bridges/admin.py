da3b29c355aa6bbdeda6d2edf9bdca0bb157524d
mamix1116 <mamix1116@gmail.com>
2021-01-23 15:47:43 +0900

    register Inspection on admin

--- app/bridges/admin.py
+++ app/bridges/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bridge, ManagementOrganization
+from .models import Bridge, ManagementOrganization, Inspection
 
 admin.site.register(ManagementOrganization)
 admin.site.register(Bridge)
+admin.site.register(Inspection)
