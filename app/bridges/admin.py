8be41269735bd7f951524eb3a256b03dd5d182a1
mamix1116 <mamix1116@gmail.com>
2021-01-27 02:00:05 +0900

    create damage model

--- app/bridges/admin.py
+++ app/bridges/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Bridge, ManagementOrganization, Inspection
+from .models import Bridge, ManagementOrganization, Inspection, Damage
 
 admin.site.register(ManagementOrganization)
 admin.site.register(Bridge)
 admin.site.register(Inspection)
+admin.site.register(Damage)
